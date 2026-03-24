dd2e312f7750d162a7e35d9aa88bc2952dcd11ab
Faruq Shobola <shobolafaruq@gmail.com>
2026-03-24 18:48:02 +0100

    Add an express app intialization

---
+++ app.js
@@ -0,0 +1,21 @@
+const path = require("path");
+
+const express = require("express");
+
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/home");
+
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+
+app.use("/admin", adminRoutes);
+app.use(shopRoutes);
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+});
+
+app.listen(3000);
