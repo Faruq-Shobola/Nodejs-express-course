48574deb81c17035e07001b0a749629fe1c6528b
Faruq Shobola <faruqshobola@gmail.com>
2026-04-15 19:42:25 +0100

    Remove express-handlebars setup and revert view engine to EJS

--- app.js
+++ app.js
@@ -2,21 +2,11 @@ const path = require("path");
 
 const express = require("express");
 
-const { engine } = require("express-handlebars");
-
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/home");
 
 const app = express();
 
-app.engine(
-  "hbs",
-  engine({
-    extname: "hbs",
-    defaultLayout: 'main-layout',
-    layoutsDir: 'views/layouts'
-  }),
-);
 app.set("view engine", "ejs");
 app.set("views", "views");
 
