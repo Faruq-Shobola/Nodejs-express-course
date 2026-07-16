9e086615dad47385757fbb25d1298082aaab96b9
Faruq Shobola <faruqshobola@gmail.com>
2026-07-16 11:04:25 +0100

    feat: add MySQL database connection and update dependencies

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/home");
 const Cart = require("./models/cart");
 
+const db = require("./utils/database");
+
+db.execute("SELECT * FROM products");
 const app = express();
 
 app.set("view engine", "ejs");
@@ -14,11 +17,11 @@ app.set("views", "views");
 app.use(express.urlencoded({ extended: false }));
 
 app.use((req, res, next) => {
-  const cart = Cart.getCart()
-  const cartCount = cart.products.reduce((count, p)=> count + p.qty, 0)
-  res.locals.cartCount = cartCount
-  next()
-})
+  const cart = Cart.getCart();
+  const cartCount = cart.products.reduce((count, p) => count + p.qty, 0);
+  res.locals.cartCount = cartCount;
+  next();
+});
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
@@ -26,7 +29,7 @@ app.use(shopRoutes);
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use((req, res, next) => {
-  res.status(404).render("404", { docTitle: "404 Not Found", path: '/404' });
+  res.status(404).render("404", { docTitle: "404 Not Found", path: "/404" });
 });
 
 app.listen(3000);
---
+++ utils/database.js
@@ -0,0 +1,11 @@
+const mysql = require('mysql2')
+
+const pool = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    database: 'artisan',
+    password: 'password'
+
+})
+
+module.exports = pool.promise()
