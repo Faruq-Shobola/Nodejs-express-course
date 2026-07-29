9fe7c1f9901caf149c44ccaf8adca488ac7b2f37
Faruq Shobola <faruqshobola@gmail.com>
2026-07-29 12:32:48 +0100

    refactor: update app to use Sequelize for database interactions

--- app.js
+++ app.js
@@ -5,8 +5,7 @@ const express = require("express");
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/home");
 const Cart = require("./models/cart");
-
-const sequelize = require('./utils/database')
+const sequelize = require("./utils/database");
 
 const app = express();
 
@@ -16,11 +15,11 @@ app.set("views", "views");
 app.use(express.urlencoded({ extended: false }));
 
 app.use((req, res, next) => {
-  const cart = Cart.getCart()
-  const cartCount = cart.products.reduce((count, p)=> count + p.qty, 0)
-  res.locals.cartCount = cartCount
-  next()
-})
+  const cart = Cart.getCart();
+  const cartCount = cart.products.reduce((count, p) => count + p.qty, 0);
+  res.locals.cartCount = cartCount;
+  next();
+});
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
@@ -28,16 +27,15 @@ app.use(shopRoutes);
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use((req, res, next) => {
-  res.status(404).render("404", { docTitle: "404 Not Found", path: '/404' });
+  res.status(404).render("404", { docTitle: "404 Not Found", path: "/404" });
 });
 
-app.listen(3000);
-
-(async ()=> {
-  try {
-  await sequelize.authenticate();
-  console.log('Connection has been established successfully.');
-} catch (error) {
-  console.error('Unable to connect to the database:', error);
-}
-})()
+sequelize
+  .sync()
+  .then((result) => {
+    console.log("Connected");
+    app.listen(3000);
+  })
+  .catch((err) => {
+    console.log("Error occured");
+  });
--- models/product.js
+++ models/product.js
@@ -1,26 +1,30 @@
-const db = require('./../utils/database')
+const { DataTypes } = require("sequelize");
+const sequelize = require("./../utils/database");
 
-class Product {
-  constructor(title, category, price, imageURL, description) {
-    this.title = title;
-    this.category = category;
-    this.price = price;
-    this.imageURL = imageURL;
-    this.description = description;
-  }
-
-  save() {
-    return db.execute('INSERT INTO products (title, price, imageUrl, description, category) VALUES (?,?,?,?,?)', 
-      [this.title, this.price, this.imageURL, this.description,this.category])
-  }
-
-  static fetchAll() {
-    return db.execute('SELECT * FROM products')
-  }
-
-  static findById(id) {
-    return db.execute('SELECT * FROM products WHERE products.id = ?', [id])
-  }
-}
+const Product = sequelize.define("product", {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true,
+  },
+  title: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  price: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  description: DataTypes.TEXT("long"),
+  category: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  imageUrl: {
+    type: DataTypes.STRING,
+    defaultValue: "default.png",
+  },
+});
 
 module.exports = Product;
