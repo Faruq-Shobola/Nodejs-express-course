017e8bebd4f7ff15fa3d98a69888abe21eea0f4f
Faruq Shobola <faruqshobola@gmail.com>
2026-09-03 18:17:17 +0100

    feat: Refactor cart functionality to use user model and update cart view

--- controllers/cart.js
+++ controllers/cart.js
@@ -1,56 +1,53 @@
-const Cart = require("./../models/cart");
-const Product = require("./../models/product")
+// const Cart = require("./../models/cart");
+const Product = require("./../models/product");
 
 const getCart = (req, res, next) => {
-    const cart = Cart.getCart();
-
-    const cartProducts = []
-
-    for(let item of cart.products) {
-        const product = Product.findById(item.id)
-        if(product) {
-            cartProducts.push({productData: product, qty: item.qty })
-        }
-    }
-
-  res.render("cart", {
-    docTitle: "Your Cart",
-    path: "/cart",
-    products: cartProducts,
-    totalPrice: cart.totalPrice
+  req.user.populate("cart.items.productId").then((user) => {
+    res.render("cart", {
+      docTitle: "Your Cart",
+      path: "/cart",
+      products: user.cart.items,
+      // totalPrice: cart.totalPrice,
+    });
   });
+
 };
 
 const postCart = (req, res, next) => {
   const prodId = req.body.productId;
-  const qty = req.body.quantity;
 
-  Cart.addProduct(prodId, qty);
-  res.redirect("/cart");
+  Product.findById(prodId)
+    .then((product) => {
+      return req.user.addToCart(product);
+    })
+    .then((result) => {
+      console.log("Product Added to cart", result);
+      res.redirect("/cart");
+    });
 };
 
 const postCartIncrease = (req, res, next) => {
-  const prodId = req.body.productId
-  Cart.increaseProduct(prodId)
-  res.redirect('/cart')
-}
+  const prodId = req.body.productId;
+  Cart.increaseProduct(prodId);
+  res.redirect("/cart");
+};
 
 const postCartDecrease = (req, res, next) => {
-  const prodId = req.body.productId
-  Cart.decreaseProduct(prodId)
-  res.redirect('/cart')
-}
+  const prodId = req.body.productId;
+  Cart.decreaseProduct(prodId);
+  res.redirect("/cart");
+};
 
 const postDelete = (req, res, next) => {
-  const prodId = req.body.productId
-  Cart.deleteProduct(prodId)
-  res.redirect('/cart')
-}
+  const prodId = req.body.productId;
+  Cart.deleteProduct(prodId);
+  res.redirect("/cart");
+};
 
 module.exports = {
   getCart,
   postCart,
   postCartIncrease,
   postCartDecrease,
-  postDelete
+  postDelete,
 };
--- routes/home.js
+++ routes/home.js
@@ -2,7 +2,7 @@ const path = require('path')
 
 const express = require("express");
 const productController = require('./../controllers/product')
-// const cartController = require('./../controllers/cart')
+const cartController = require('./../controllers/cart')
 
 const router = express.Router();
 
@@ -18,8 +18,8 @@ router.get("/products/:productId", productController.getProduct);
 
 
 // cart route
-// router.get("/cart", cartController.getCart);
-// router.post("/cart", cartController.postCart);
+router.get("/cart", cartController.getCart);
+router.post("/cart", cartController.postCart);
 // router.post('/cart/increase', cartController.postCartIncrease)
 // router.post('/cart/decrease', cartController.postCartDecrease)
 // router.post('/cart/delete', cartController.postDelete)
